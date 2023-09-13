73cfeae6370b8a8ff6d8db4023fccb6e38f096ba
foxydepiraat <foxydepiraat@gmail.com>
2023-09-13 08:33:58 +0200

    tutorial is almost done

---
+++ textbasedAdventure/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textbasedAdventure
+{
+    internal class Player
+    {
+        public int hp;
+        public int maxHp;
+        public int dam;
+        public int def;
+        public int coins;
+        public int Pos;
+        public void playerStart()
+        {
+            this.hp = 30;
+            this.maxHp = 30;
+            this.dam = 5;
+            this.def = 0;
+            this.coins = 0;
+            this.Pos = 0;
+
+        }
+    }
+}
--- textbasedAdventure/WindowInfo.xaml.cs
+++ textbasedAdventure/WindowInfo.xaml.cs
@@ -19,10 +19,12 @@ namespace textbasedAdventure
     /// </summary>
     public partial class WindowInfo : Window
     {
-        private string Default = "hover over something to know what it means";
+        private string Default = "Hover over something to know what it means.";
+        private string Options = "This is button wil make you move or an attack move or for the shop to buy.";
         public WindowInfo()
         {
             InitializeComponent();
+            txtTutorial.Text = Default;
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
@@ -32,7 +34,7 @@ namespace textbasedAdventure
 
         private void txtUsername_MouseEnter(object sender, MouseEventArgs e)
         {
-            txtTutorial.Text = "This is your username";
+            txtTutorial.Text = "This is your username nothing special.";
         }
 
         private void txtUsername_MouseLeave(object sender, MouseEventArgs e)
@@ -42,7 +44,7 @@ namespace textbasedAdventure
 
         private void txtHp_MouseEnter(object sender, MouseEventArgs e)
         {
-            txtTutorial.Text = "This shows how much hp you have left out of maximum";
+            txtTutorial.Text = "This shows how much hp you have left out of maximum.";
         }
 
         private void txtHp_MouseLeave(object sender, MouseEventArgs e)
@@ -52,7 +54,7 @@ namespace textbasedAdventure
 
         private void txtDam_MouseEnter(object sender, MouseEventArgs e)
         {
-            txtTutorial.Text = "This is how much damage you would to to the enemy";
+            txtTutorial.Text = "This is how much damage you would to to the enemy.";
         }
 
         private void txtDam_MouseLeave(object sender, MouseEventArgs e)
@@ -62,7 +64,7 @@ namespace textbasedAdventure
 
         private void txtDef_MouseEnter(object sender, MouseEventArgs e)
         {
-            txtTutorial.Text = "This is how much defense you have that removes the damage from the enemy";
+            txtTutorial.Text = "This is how much defense you have that removes the damage from the enemy.";
         }
 
         private void txtDef_MouseLeave(object sender, MouseEventArgs e)
@@ -72,7 +74,7 @@ namespace textbasedAdventure
 
         private void txtCoins_MouseEnter(object sender, MouseEventArgs e)
         {
-            txtTutorial.Text = "this is how much coins you have with these coins you can buy hp, damage or defense";
+            txtTutorial.Text = "this is how much coins you have with these coins you can buy hp, damage or defense coins are gained from killing enemys or finding special locations.";
         }
 
         private void txtCoins_MouseLeave(object sender, MouseEventArgs e)
@@ -80,5 +82,76 @@ namespace textbasedAdventure
             txtTutorial.Text = Default;
         }
 
+        private void btnOption1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = Options;
+        }
+
+        private void btnOption1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = Default;
+        }
+        private void btnOption2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = Options;
+        }
+
+        private void btnOption2_MouseLeave(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = Default;
+        }
+
+        private void btnOption3_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = Options;
+        }
+
+        private void btnOption3_MouseLeave(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = Default;
+        }
+        private void btnOption4_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = Options;
+        }
+
+        private void btnOption4_MouseLeave(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = Default;
+        }
+
+        private void btnShop_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = "to go to the shop to buy upgrades and you wont be able to buy upgrades in a fight.";
+        }
+
+        private void btnShop_MouseLeave(object sender, MouseEventArgs e)
+        {
+            txtTutorial.Text = Default;
+        }
+
+        private void btnShop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtTutorial_MouseEnter(object sender,EventArgs e)
+        {
+            txtTutorial.Text = "This is where the story wil be told.";
+        }
+        private void txtTutorial_MouseLeave(object sender, EventArgs e)
+        {
+            txtTutorial.Text = Default;
+        }
+
+        private void txtWin_MouseEnter(object sender,MouseEventArgs e)
+        {
+            txtTutorial.Text = "you need to survive 10 moves until you have reached .....";
+        }
+
+        private void txtWin_MouseLeave(object sender,MouseEventArgs e)
+        {
+            txtTutorial.Text = Default;
+        }
     }
 }
